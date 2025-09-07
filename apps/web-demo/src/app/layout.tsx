592a638532ab761fe9646815d27c66b4b6095e48
xn1cklas <nicklas.scharpff@nsquare.one>
2025-09-07 15:31:47 +0200

    fix(layout): set HTML lang attribute to 'en' and add 'dark' class for improved styling

--- apps/web-demo/src/app/layout.tsx
+++ apps/web-demo/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col items-center justify-center min-h-screen`}
       >
